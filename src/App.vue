e6214e7be6d77221748c0718078806bf70b3f1f2
Miguel Cabrera <miguelcabsan@gmail.com>
2019-04-04 01:03:35 +0100

    NONE - Preparing project
    > Adding Husky scripts for pre-commit hook
    
    > Adding ibm plex font as main font project
    
    > Adding .prettierignore file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
     <h1>¡VueJS 2.0!</h1>
-    <img src="./assets/logo.png" alt="VueJS Logo">
+    <img src="./assets/images/logo.png" alt="VueJS Logo" />
   </div>
 </template>
 
-<style>
-  :root {
-    text-align: center;
-  }
+<style lang="postcss">
+@import url('../node_modules/@ibm/plex/css/ibm-plex.css');
+:root {
+  text-align: center;
+  font-family: 'IBM Plex Sans', Impact, sans-serif;
+}
 
-  h1 {
-    font-family: Montserrat, Impact, sans-serif;
-    font-size: 72px;
-  }
+h1 {
+  font-size: 72px;
+}
 </style>
